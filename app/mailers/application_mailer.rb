bdd02635c5db6944420bd8f83678e502e2bf9422
Setiadi <setiadi.seth@gmail.com>
2023-05-25 11:48:32 +0700

    add mailer setting

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  def sample_email
+    mail(to: 'recipient@example.com', subject: 'Test Email')
+    deliver_now
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mailgun.org',
+    port: 587,
+    domain: ENV['MAILGUN_DOMAIN'],
+    user_name: 'postmaster@' + ENV['MAILGUN_DOMAIN'],
+    password: ENV['MAILGUN_API_KEY'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  config.action_mailer.raise_delivery_errors = true
 end
